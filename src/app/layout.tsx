1c1ec7a88fe38d1f908f100da03421849715a03b
leechiahwa <leechiahwa99@hotmail.com>
2025-04-10 17:34:53 +0800

    Add chatbot functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Chatbot from "@/components/chatbot";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Chatbot />
         <Footer />
       </body>
     </html>
