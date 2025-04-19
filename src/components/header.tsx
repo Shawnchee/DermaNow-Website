e99ef40c730409032ec60afaf1b2cde92e9c272b
leechiahwa <leechiahwa99@hotmail.com>
2025-04-20 03:03:24 +0800

    Add gitbook link to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -197,8 +197,10 @@ export default function Header() {
                 </span>
                 <div className="absolute left-0 mt-2 w-56 bg-white border border-gray-200 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition duration-200 z-50">
                   <Link
-                    href="/documentation"
+                    href="https://dermanow.gitbook.io/dermanow-docs"
                     className="block px-4 py-2 text-gray-700 hover:text-blue-600"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Documentation/Guide
                   </Link>
